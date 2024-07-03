71d8d7493ca8f5ec1d71544662fe67e086627a7f
Kripakoff <info@micom.by>
2024-07-03 19:06:16 +0300

    feat: add lint, prettier, husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaFeatures: {
+      "jsx": true
+    },
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  env: {browser: true, es2020: true},
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "prettier",
+    "plugin:prettier/recommended"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    "react-compiler", "@typescript-eslint", "react", "prettier",
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    "comma-dangle": ["error", "only-multiline"],
+    "prettier/prettier": ["error", {"endOfLine": "auto"}],
+  },
+};
